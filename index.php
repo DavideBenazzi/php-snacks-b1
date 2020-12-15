698d96a51538f2af67a85cf73a0d67034c85f379
Davide Benazzi <benazzi.davide@gmail.com>
2020-12-15 16:51:48 +0100

    Check name and age power increase

--- index.php
+++ index.php
@@ -23,15 +23,15 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
     <h2>
         <?php       
         // CHECK VALIDITY NAME
-            if ( strlen($_GET['name']) < 4 ) {
+            if ( ( strlen( trim($_GET['name']) ) < 4 ) || ( strpos($_GET['name'] , ' ') ) ) {
                 echo ("Accesso negato, inserire un 'name' valido.");
             }
             // CHECK MAIL
             else if ( (! strpos($_GET['mail'] , '@') ) || (! strpos($_GET['mail'] , '.') ) ) {
                 echo ("Accesso negato, inserire una 'mail' valida.");
             }
-            // CHECK AGE
-            else if (! ctype_digit($_GET['age']) ) {
+            // CHECK AGE (FROM 0 TO 120)
+            else if ( (! ctype_digit($_GET['age']) ) || (! ( ( $_GET['age'] < 120) && ( $_GET['age'] > 0) ) ) ) {
                 echo ("Accesso negato, inserire una 'age' valida.");
             }       
             else {
